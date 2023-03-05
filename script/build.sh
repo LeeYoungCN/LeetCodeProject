8efc79ea3f3b6f23dd5c351d40dc52c1fba0b983
LiYang <1175529741@qq.com>
2023-03-05 11:23:00 +0800

    Darwin

--- script/build.sh
+++ script/build.sh
@@ -4,6 +4,7 @@ root_path=$(cd ${file_path}/..; pwd)
 buildcache_path="${root_path}/buildcache"
 bin_path="${buildcache_path}/bin"
 target_name="LeetCodePractice"
+cmake_toolchain_file="${root_path}/cmake/project_toolchain.cmake"
 os=$(uname -s)
 generator="Unix Makefiles"
 if [[ ${os} =~ "MINGW" ]]; then
@@ -18,7 +19,7 @@ fi
 mkdir -p "${buildcache_path}"
 
 cd "${root_path}" || pwd
-cmake -S "${root_path}" -B "${buildcache_path}" -G "${generator}"
+cmake -S "${root_path}" -B "${buildcache_path}" -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -G "${generator}"
 cmake --build "${buildcache_path}" --target "${target_name}"
 
 cd "${bin_path}" || pwd
