f198eeb54ceb19e576b76e34e2ee7854b6de8f70
ly <1175529741@qq.com>
2025-05-18 15:10:10 +0800

    linux

--- template/leetcode_src_file.cpp
+++ template/leetcode_src_file.cpp
@@ -1,10 +1,14 @@
 // URL_STR
+#include <cstdint>
+#include <cmath>
 #include <vector>
+#include <algorithm>
 #include "HEAD_FILE_NAME"
 
 using namespace std;
 
-int CLASS_NAME::LeetCodeFunction(std::vector<int> x) {
+int CLASS_NAME::LeetCodeFunction(std::vector<int> x)
+{
     return 0;
 }
 
