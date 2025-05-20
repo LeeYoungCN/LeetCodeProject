e7b07ba95652ef5ceb10e14ced813fc021a9ca0e
ly <1175529741@qq.com>
2025-05-20 16:26:34 +0800

    lc0152

---
+++ leetcode/include/lc0152_maximum_product_subarray.h
@@ -0,0 +1,15 @@
+/*
+ * Time : 2025-05-20 15:14:30
+ * URL  : https://leetcode.cn/problems/maximum-product-subarray/
+ */
+
+#ifndef LC00152_MAXIMUM_PRODUCT_SUBARRAY_H
+#define LC00152_MAXIMUM_PRODUCT_SUBARRAY_H
+#include <vector>
+
+class LC00152_MaximumProductSubarray {
+    public:
+        int maxProduct(std::vector<int>& nums);
+};
+
+#endif // LC00152_MAXIMUM_PRODUCT_SUBARRAY_H
---
+++ leetcode/src/lc0152_maximum_product_subarray.cpp
@@ -0,0 +1,36 @@
+/*
+ * Time    : 2025-05-20 15:14:30
+ * URL     : https://leetcode.cn/problems/maximum-product-subarray/
+ * Problem : 给你一个整数数组 nums ，请你找出数组中乘积最大的非空连续 子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ *           测试用例的答案是一个 32-位 整数。
+ */
+#include <cstdint>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include "lc0152_maximum_product_subarray.h"
+
+using namespace std;
+
+int LC00152_MaximumProductSubarray::maxProduct(vector<int>& nums)
+{
+    int ret = nums[0];
+    uint32_t len = nums.size();
+    vector<int32_t> dpMax(len);
+    vector<int32_t> dpMin(len);
+
+    for (uint32_t i = 0; i < len; ++i) {
+        int32_t n = nums[i];
+        if (i == 0) {
+            dpMax[i] = dpMin[i] = n;
+        } else {
+            dpMax[i] = max(n, max(dpMax[i - 1] * n, dpMin[i - 1] * n));
+            dpMin[i] = min(n, min(dpMax[i - 1] * n, dpMin[i - 1] * n));
+        }
+        ret = max(dpMax[i], ret);
+    }
+    return ret;
+}
+
