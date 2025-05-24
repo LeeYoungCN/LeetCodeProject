72b09209ab5675f028066b91c5af7c2785a121b7
ly <1175529741@qq.com>
2025-05-24 12:44:43 +0800

    lc0337

---
+++ leetcode/include/lc0337_house_robber_iii.h
@@ -0,0 +1,30 @@
+/*
+ * Time : 2025-05-24 11:30:31
+ * URL  : https://leetcode.cn/problems/house-robber-iii/description/
+ */
+#ifndef LC0337_HOUSE_ROBBER_III_H
+#define LC0337_HOUSE_ROBBER_III_H
+
+#include <deque>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class LC0337_HouseRobberIII {
+    public:
+        LC0337_HouseRobberIII() = default;
+        virtual ~LC0337_HouseRobberIII() = default;
+        int rob(TreeNode *root);
+};
+
+#endif  // LC0337_HOUSE_ROBBER_III_H
