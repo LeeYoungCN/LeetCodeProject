aa36ebabdd187eb8a70e8daabb50e75a94a9ab92
ly <1175529741@qq.com>
2025-05-24 14:20:31 +0800

    lc0198

---
+++ leetcode/include/lc0198_house_robber.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-05-24 13:37:13
+ * URL  : https://leetcode.cn/problems/house-robber/
+ */
+#ifndef LC0198_HOUSE_ROBBER_H
+#define LC0198_HOUSE_ROBBER_H
+
+#include <vector>
+
+class LC0198_HouseRobber {
+    public:
+        LC0198_HouseRobber() = default;
+        virtual ~LC0198_HouseRobber() = default;
+        int rob(std::vector<int>& nums);
+};
+
+#endif  // LC0198_HOUSE_ROBBER_H
