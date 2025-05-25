48eecd2a4649d215cbddcc1c7a8dad350f4e4610
ly <1175529741@qq.com>
2025-05-25 11:31:10 +0800

    lc2131

---
+++ leetcode/include/lc2131_longest_palindrome_by_concatenating_two_letter_words.h
@@ -0,0 +1,19 @@
+/*
+ * Time : 2025-05-25 08:55:42
+ * URL  :
+ * https://leetcode.cn/problems/longest-palindrome-by-concatenating-two-letter-words/?envType=daily-questionURL_STRenvId=2025-05-25
+ */
+#ifndef LC2131_LONGEST_PALINDROME_BY_CONCATENATING_TWO_LETTER_WORDS_H
+#define LC2131_LONGEST_PALINDROME_BY_CONCATENATING_TWO_LETTER_WORDS_H
+
+#include <string>
+#include <vector>
+
+class LC2131_LongestPalindromeByConcatenatingTwoLetterWords {
+public:
+    LC2131_LongestPalindromeByConcatenatingTwoLetterWords() = default;
+    virtual ~LC2131_LongestPalindromeByConcatenatingTwoLetterWords() = default;
+    int longestPalindrome(std::vector<std::string>& words);
+};
+
+#endif  // LC2131_LONGEST_PALINDROME_BY_CONCATENATING_TWO_LETTER_WORDS_H
---
+++ leetcode/src/lc2131_longest_palindrome_by_concatenating_two_letter_words.cpp
@@ -0,0 +1,50 @@
+/*
+ * Time : 2025-05-25 08:55:42
+ * URL  :
+ * https://leetcode.cn/problems/longest-palindrome-by-concatenating-two-letter-words/?envType=daily-questionURL_STRenvId=2025-05-25
+ */
+#include "lc2131_longest_palindrome_by_concatenating_two_letter_words.h"
+
+#include <algorithm>
+#include <cstdint>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int LC2131_LongestPalindromeByConcatenatingTwoLetterWords::longestPalindrome(vector<string> &words)
+{
+    map<string, int32_t> wordMap;
+
+    for (const string &word : words) {
+        wordMap[word]++;
+    }
+    bool haveMid = false;
+    int32_t ret = 0;
+    for (const auto &[word, cnt] : wordMap) {
+        if (cnt == 0) {
+            continue;
+        }
+
+        if (word[0] == word[1]) {
+            ret += (cnt / 2) * 4;
+            if (cnt % 2 == 1 && !haveMid) {
+                ret += 2;
+                haveMid = true;
+            }
+            continue;
+        }
+
+        string revWord = {word[1], word[0]};
+        if (wordMap[revWord] == 0) {
+            continue;
+        }
+
+        int32_t n = min(wordMap[revWord], cnt);
+        ret += (4 * n);
+        wordMap[revWord] = 0;
+    }
+    return ret;
+}
