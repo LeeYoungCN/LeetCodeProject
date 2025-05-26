5da2f4d648f3fbe5aecdc25ffaf3b199ae0edbb0
ly <1175529741@qq.com>
2025-05-27 00:32:40 +0800

    lc2894

---
+++ leetcode/include/lc2894_divisible_and_non_divisible_sums_difference.h
@@ -0,0 +1,16 @@
+/*
+ * Time : 2025-05-27 00:20:24
+ * URL  :
+ * https://leetcode.cn/problems/divisible-and-non-divisible-sums-difference/description/?envType=daily-questionURL_STRenvId=2025-05-27
+ */
+#ifndef LC2894_DIVISIBLE_AND_NON_DIVISIBLE_SUMS_DIFFERENCE_H
+#define LC2894_DIVISIBLE_AND_NON_DIVISIBLE_SUMS_DIFFERENCE_H
+
+class LC2894_DivisibleAndNonDivisibleSumsDifference {
+public:
+    LC2894_DivisibleAndNonDivisibleSumsDifference() = default;
+    virtual ~LC2894_DivisibleAndNonDivisibleSumsDifference() = default;
+    int differenceOfSums(int n, int m);
+};
+
+#endif  // LC2894_DIVISIBLE_AND_NON_DIVISIBLE_SUMS_DIFFERENCE_H
