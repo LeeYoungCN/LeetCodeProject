8d2081feec301e602470148aa072de9615e4e315
ly <1175529741@qq.com>
2025-05-27 12:30:16 +0800

    lc3068 dfs

--- leetcode/src/lc3068_find_the_maximum_sum_of_node_values.cpp
+++ leetcode/src/lc3068_find_the_maximum_sum_of_node_values.cpp
@@ -1,30 +1,23 @@
 /*
  * Time     : 2025-05-23 01:03:47
- * URL      : https://leetcode.cn/problems/find-the-maximum-sum-of-node-values/description/?envType=daily-questionURL_STRenvId=2025-05-23
- * Problem  : 给你一棵 n 个节点的无向树，节点从 0 到 n - 1 编号。树以长度为 n - 1 下标从 0 开始的二维整数数组 edges 的形式给你，
- *            其中 edges[i] = [ui, vi] 表示树中节点 ui 和 vi 之间有一条边。
- *            同时给你一个 正 整数 k 和一个长度为 n 下标从 0 开始的 非负 整数数组 nums ，其中 nums[i] 表示节点 i 的 价值 。
- *            Alice 想 最大化 树中所有节点价值之和。为了实现这一目标，Alice 可以执行以下操作任意次（包括 0 次）：
- *            选择连接节点 u 和 v 的边 [u, v]，并将它们的值更新为：
- *            nums[u] = nums[u] XOR k
- *            nums[v] = nums[v] XOR k
- *            请你返回 Alice 通过执行以上操作任意次后，可以得到所有节点价值之和的最大值 。
+ * URL      :
+ * https://leetcode.cn/problems/find-the-maximum-sum-of-node-values/description/?envType=daily-questionURL_STRenvId=2025-05-23
+ * Problem  : 给你一棵 n 个节点的无向树，节点从 0 到 n - 1 编号。树以长度为 n - 1 下标从 0 开始的二维整数数组 edges
+ * 的形式给你， 其中 edges[i] = [ui, vi] 表示树中节点 ui 和 vi 之间有一条边。 同时给你一个 正 整数 k 和一个长度为 n
+ * 下标从 0 开始的 非负 整数数组 nums ，其中 nums[i] 表示节点 i 的 价值 。 Alice 想 最大化
+ * 树中所有节点价值之和。为了实现这一目标，Alice 可以执行以下操作任意次（包括 0 次）： 选择连接节点 u 和 v 的边 [u,
+ * v]，并将它们的值更新为： nums[u] = nums[u] XOR k nums[v] = nums[v] XOR k 请你返回 Alice
+ * 通过执行以上操作任意次后，可以得到所有节点价值之和的最大值 。
  */
+#include "lc3068_find_the_maximum_sum_of_node_values.h"
+
+#include <algorithm>
+#include <cctype>
+#include <cmath>
 #include <cstdint>
 #include <cstdio>
-#include <cmath>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <queue>
-#include <deque>
-#include <set>
-#include <stack>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
 #include <functional>
-#include "lc3068_find_the_maximum_sum_of_node_values.h"
+#include <vector>
 
 // 一个值 a 异或偶数次 k 值仍为 a。
 // 对于树上任意两点，存在一条路径，对路径上的每一条边进行操作，除了路径的起点和终点进行了一次异或，剩下的所有点都进行了两次异或，值不变。也就是等价于我们可以对树上任意两点进行一次异或操作。
@@ -32,7 +25,7 @@
 using namespace std;
 
 long long LC3068_FindTheMaximumSumOfNodeValues_Greedy::maximumValueSum(vector<int> &nums, int k,
-                                                                       vector<vector<int> > &edges)
+                                                                       vector<vector<int>> &edges)
 {
     const uint32_t length = nums.size();
 
@@ -58,9 +51,51 @@ long long LC3068_FindTheMaximumSumOfNodeValues_Greedy::maximumValueSum(vector<in
 }
 
 long long LC3068_FindTheMaximumSumOfNodeValues_DFS::maximumValueSum(std::vector<int> &nums, int k,
-                                                                    std::vector<std::vector<int> > &edges)
+                                                                    std::vector<std::vector<int>> &edges)
 {
-    uint32_t length = nums.size();
-    
-    return 0;
+    const uint32_t START_IDX = 0;
+    const uint32_t END_IDX = 1;
+
+    uint32_t totalNode = nums.size();
+    vector<vector<int32_t>> grid(totalNode);
+
+    for (const vector<int32_t> &edge : edges) {
+        int32_t start = edge[START_IDX];
+        int32_t end = edge[END_IDX];
+        grid[start].push_back(end);
+        grid[end].push_back(start);
+    }
+
+    vector<vector<int64_t>> dp = {
+        vector<int64_t>(totalNode, 0),         // not flip
+        vector<int64_t>(totalNode, INT64_MIN)  // flip
+    };
+
+    const uint32_t NOT_FLIP_IDX = 0;
+    const uint32_t FLIP_IDX = 1;
+
+    function<void(int32_t, int32_t)> dfs = [&](int32_t curr, int32_t parent) {
+        for (int32_t child : grid[curr]) {
+            if (child == parent) {
+                continue;
+            }
+            dfs(child, curr);
+            int64_t notFlip =
+                max(dp[FLIP_IDX][curr] + dp[FLIP_IDX][child], dp[NOT_FLIP_IDX][curr] + dp[NOT_FLIP_IDX][child]);
+            int64_t flip =
+                max(dp[NOT_FLIP_IDX][curr] + dp[FLIP_IDX][child], dp[FLIP_IDX][curr] + dp[NOT_FLIP_IDX][child]);
+
+            dp[NOT_FLIP_IDX][curr] = notFlip;
+            dp[FLIP_IDX][curr] = flip;
+        }
+
+        int64_t oldNotflip = dp[NOT_FLIP_IDX][curr];
+        int64_t oldFlip = dp[FLIP_IDX][curr];
+
+        dp[NOT_FLIP_IDX][curr] = max(oldNotflip + nums[curr], oldFlip + (nums[curr] ^ k));
+        dp[FLIP_IDX][curr] = max(oldNotflip + (nums[curr] ^ k), oldFlip + nums[curr]);
+    };
+
+    dfs(0, -1);
+    return dp[NOT_FLIP_IDX][0];
 }
--- test/test_lc3068_find_the_maximum_sum_of_node_values.cpp
+++ test/test_lc3068_find_the_maximum_sum_of_node_values.cpp
@@ -1,42 +1,43 @@
 /*
  * Time : 2025-05-23 01:03:47
- * URL  : https://leetcode.cn/problems/find-the-maximum-sum-of-node-values/description/?envType=daily-questionURL_STRenvId=2025-05-23
+ * URL  :
+ * https://leetcode.cn/problems/find-the-maximum-sum-of-node-values/description/?envType=daily-questionURL_STRenvId=2025-05-23
  */
 #include <vector>
+
 #include "gtest/gtest.h"
 #include "lc3068_find_the_maximum_sum_of_node_values.h"
 
 using namespace std;
 
 class TEST_LC3068 : public testing::Test {
-    protected:
-        static void SetUpTestSuite() {}
-        static void TearDownTestSuite() {}
-        void SetUp() override;
-        void TearDown() override;
+protected:
+    static void SetUpTestSuite() {}
+    static void TearDownTestSuite() {}
+    void SetUp() override;
+    void TearDown() override;
 
-        vector<LC3068_FindTheMaximumSumOfNodeValues *> m_testList;
-        void RunTest(vector<int>& nums, int k, vector<vector<int>>& edges,
-                     long long expectResult);
+    vector<LC3068_FindTheMaximumSumOfNodeValues*> m_testList;
+    void RunTest(vector<int>& nums, int k, vector<vector<int>>& edges, long long expectResult);
 };
 
 void TEST_LC3068::SetUp()
 {
     m_testList.push_back(new LC3068_FindTheMaximumSumOfNodeValues_Greedy());
+    m_testList.push_back(new LC3068_FindTheMaximumSumOfNodeValues_DFS());
 }
 
 void TEST_LC3068::TearDown()
 {
-    for (LC3068_FindTheMaximumSumOfNodeValues *inst : m_testList) {
+    for (LC3068_FindTheMaximumSumOfNodeValues* inst : m_testList) {
         delete inst;
     }
 }
 
-void TEST_LC3068::RunTest(vector<int>& nums, int k, vector<vector<int>>& edges,
-                          long long expectResult)
+void TEST_LC3068::RunTest(vector<int>& nums, int k, vector<vector<int>>& edges, long long expectResult)
 {
-    for (LC3068_FindTheMaximumSumOfNodeValues *inst : m_testList) {
-        EXPECT_EQ(expectResult, inst->maximumValueSum(nums, k, edges));
+    for (LC3068_FindTheMaximumSumOfNodeValues* inst : m_testList) {
+        EXPECT_EQ(expectResult, inst->maximumValueSum(nums, k, edges)) << typeid(inst).name();
     }
 }
 
