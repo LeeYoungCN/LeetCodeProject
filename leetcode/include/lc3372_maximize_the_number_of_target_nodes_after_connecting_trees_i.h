f338db5f06567ef9378ed788434d30426a83b855
ly <1175529741@qq.com>
2025-05-28 22:18:31 +0800

    lc3372

---
+++ leetcode/include/lc3372_maximize_the_number_of_target_nodes_after_connecting_trees_i.h
@@ -0,0 +1,24 @@
+/*
+ * Time : 2025-05-28 14:14:32
+ * URL  :
+ * https://leetcode.cn/problems/maximize-the-number-of-target-nodes-after-connecting-trees-i/description/?envType=daily-questionURL_STRenvId=2025-05-28
+ */
+#ifndef LC3372_MAXIMIZE_THE_NUMBER_OF_TARGET_NODES_AFTER_CONNECTING_TREES_I_H
+#define LC3372_MAXIMIZE_THE_NUMBER_OF_TARGET_NODES_AFTER_CONNECTING_TREES_I_H
+
+#include <vector>
+
+class LC3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI {
+public:
+    LC3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI() = default;
+    virtual ~LC3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI() = default;
+    std::vector<int> maxTargetNodes(std::vector<std::vector<int>>& edges1, std::vector<std::vector<int>>& edges2,
+                                    int k);
+
+private:
+    std::vector<std::vector<int>> BuildGraph(const std::vector<std::vector<int>>& edges);
+    int GetKNodeCnt(const std::vector<std::vector<int>>& graph, int k, int nodeIdx, int parent);
+    std::vector<int> GetKNodeList(const std::vector<std::vector<int>>& edges, int k);
+};
+
+#endif  // LC3372_MAXIMIZE_THE_NUMBER_OF_TARGET_NODES_AFTER_CONNECTING_TREES_I_H
---
+++ leetcode/src/lc3372_maximize_the_number_of_target_nodes_after_connecting_trees_i.cpp
@@ -0,0 +1,82 @@
+/*
+ * Time : 2025-05-28 14:14:32
+ * URL  :
+ * https://leetcode.cn/problems/maximize-the-number-of-target-nodes-after-connecting-trees-i/description/?envType=daily-questionURL_STRenvId=2025-05-28
+ */
+#include "lc3372_maximize_the_number_of_target_nodes_after_connecting_trees_i.h"
+
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int32_t>> LC3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI::BuildGraph(
+    const vector<vector<int32_t>>& edges)
+{
+    const uint32_t START_NODE = 0;
+    const uint32_t END_NODE = 1;
+
+    uint32_t nodeCnt = (uint32_t)edges.size() + 1;
+    vector<vector<int32_t>> graph(nodeCnt, vector<int32_t>());
+
+    for (const vector<int32_t>& edge : edges) {
+        int32_t start = edge[START_NODE];
+        int32_t end = edge[END_NODE];
+
+        graph[start].push_back(end);
+        graph[end].push_back(start);
+    }
+
+    return graph;
+}
+
+int32_t LC3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI::GetKNodeCnt(const vector<vector<int32_t>>& graph,
+                                                                                int32_t k, int32_t nodeIdx,
+                                                                                int32_t parent)
+{
+    if (k < 0) {
+        return 0;
+    }
+
+    int32_t ans = 1;
+
+    for (int32_t child : graph[nodeIdx]) {
+        if (child == parent) {
+            continue;
+        }
+        ans += GetKNodeCnt(graph, k - 1, child, nodeIdx);
+    }
+
+    return ans;
+}
+
+vector<int32_t> LC3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI::GetKNodeList(
+    const vector<vector<int>>& edges, int32_t k)
+{
+    uint32_t nodeCnt = edges.size() + 1;
+    vector<vector<int32_t>> graph = BuildGraph(edges);
+
+    vector<int32_t> ans(nodeCnt, 0);
+    for (uint32_t i = 0; i < nodeCnt; i++) {
+        ans[i] = GetKNodeCnt(graph, k, i, -1);
+    }
+
+    return ans;
+}
+
+vector<int> LC3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesI::maxTargetNodes(vector<vector<int>>& edges1,
+                                                                                       vector<vector<int>>& edges2,
+                                                                                       int k)
+{
+    vector<int32_t> count1 = GetKNodeList(edges1, k);
+    vector<int32_t> count2 = GetKNodeList(edges2, k - 1);
+
+    vector<int32_t>::iterator maxIt = max_element(count2.begin(), count2.end());
+
+    for (vector<int32_t>::iterator it = count1.begin(); it != count1.end(); it++) {
+        *it += *maxIt;
+    }
+
+    return count1;
+}
