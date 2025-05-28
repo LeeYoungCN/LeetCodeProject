73cd074dd516ec35fc1c1a7f1764c25d97a53205
ly <1175529741@qq.com>
2025-05-29 01:48:19 +0800

    lc3373

---
+++ leetcode/include/lc3373_maximize_the_number_of_target_nodes_after_connecting_trees_ii.h
@@ -0,0 +1,25 @@
+/*
+ * Time : 2025-05-29 00:11:51
+ * URL  :
+ * https://leetcode.cn/problems/maximize-the-number-of-target-nodes-after-connecting-trees-ii/description/?envType=daily-questionURL_STRenvId=2025-05-29
+ */
+#ifndef LC3373_MAXIMIZE_THE_NUMBER_OF_TARGET_NODES_AFTER_CONNECTING_TREES_II_H
+#define LC3373_MAXIMIZE_THE_NUMBER_OF_TARGET_NODES_AFTER_CONNECTING_TREES_II_H
+
+#include <vector>
+
+struct TreeData;
+
+class LC3373_MaximizeTheNumberOfTargetNodesAfterConnectingTreesII {
+public:
+    LC3373_MaximizeTheNumberOfTargetNodesAfterConnectingTreesII() = default;
+    virtual ~LC3373_MaximizeTheNumberOfTargetNodesAfterConnectingTreesII() = default;
+    std::vector<int> maxTargetNodes(std::vector<std::vector<int>>& edges1, std::vector<std::vector<int>>& edges2);
+
+private:
+    std::vector<std::vector<int>> BuildGraph(const std::vector<std::vector<int>>& edges);
+    void DFS(const std::vector<std::vector<int>>& graph, int depth, int curr, int parent, TreeData& treeNodeData);
+    void GetTreeNodeData(const std::vector<std::vector<int>>& edges, TreeData& treeNodeData);
+};
+
+#endif  // LC3373_MAXIMIZE_THE_NUMBER_OF_TARGET_NODES_AFTER_CONNECTING_TREES_II_H
---
+++ leetcode/src/lc3373_maximize_the_number_of_target_nodes_after_connecting_trees_ii.cpp
@@ -0,0 +1,94 @@
+/*
+ * Time : 2025-05-29 00:11:51
+ * URL  :
+ * https://leetcode.cn/problems/maximize-the-number-of-target-nodes-after-connecting-trees-ii/description/?envType=daily-questionURL_STRenvId=2025-05-29
+ */
+#include "lc3373_maximize_the_number_of_target_nodes_after_connecting_trees_ii.h"
+
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <vector>
+
+using namespace std;
+
+enum class NodeType { EVEN, ODD };
+
+struct TreeData {
+    int32_t evenCnt;
+    int32_t oddCnt;
+    vector<NodeType> nodeList;
+};
+
+vector<vector<int32_t>> LC3373_MaximizeTheNumberOfTargetNodesAfterConnectingTreesII::BuildGraph(
+    const vector<vector<int32_t>>& edges)
+{
+    const uint32_t START_NODE = 0;
+    const uint32_t END_NODE = 1;
+
+    uint32_t nodeCnt = (uint32_t)edges.size() + 1;
+    vector<vector<int32_t>> graph(nodeCnt, vector<int32_t>());
+
+    for (const vector<int32_t>& edge : edges) {
+        int32_t start = edge[START_NODE];
+        int32_t end = edge[END_NODE];
+
+        graph[start].push_back(end);
+        graph[end].push_back(start);
+    }
+
+    return graph;
+}
+
+void LC3373_MaximizeTheNumberOfTargetNodesAfterConnectingTreesII::DFS(const vector<vector<int32_t>>& graph,
+                                                                      int32_t depth, int32_t curr, int32_t parent,
+                                                                      TreeData& treeNodeData)
+{
+    if (depth % 2 == 0) {
+        treeNodeData.evenCnt++;
+        treeNodeData.nodeList[curr] = NodeType::EVEN;
+    } else {
+        treeNodeData.oddCnt++;
+        treeNodeData.nodeList[curr] = NodeType::ODD;
+    }
+
+    for (int32_t child : graph[curr]) {
+        if (child == parent) {
+            continue;
+        }
+        DFS(graph, depth + 1, child, curr, treeNodeData);
+    }
+}
+
+void LC3373_MaximizeTheNumberOfTargetNodesAfterConnectingTreesII::GetTreeNodeData(const vector<vector<int>>& edges,
+                                                                                  TreeData& treeNodeData)
+{
+    uint32_t nodeCnt = edges.size() + 1;
+    vector<vector<int32_t>> graph = BuildGraph(edges);
+
+    vector<int32_t> ans(nodeCnt, 0);
+
+    DFS(graph, 0, 0, -1, treeNodeData);
+}
+
+vector<int32_t> LC3373_MaximizeTheNumberOfTargetNodesAfterConnectingTreesII::maxTargetNodes(
+    vector<vector<int32_t>>& edges1, vector<vector<int32_t>>& edges2)
+{
+    uint32_t nodeCnt1 = edges1.size() + 1;
+    uint32_t nodeCnt2 = edges2.size() + 1;
+
+    TreeData treeData1 = {.evenCnt = 0, .oddCnt = 0, .nodeList = vector<NodeType>(nodeCnt1, NodeType::EVEN)};
+    TreeData treeData2 = {.evenCnt = 0, .oddCnt = 0, .nodeList = vector<NodeType>(nodeCnt2, NodeType::EVEN)};
+
+    GetTreeNodeData(edges1, treeData1);
+    GetTreeNodeData(edges2, treeData2);
+
+    int32_t maxTree2 = max(treeData2.evenCnt, treeData2.oddCnt);
+    vector<int32_t> ans(nodeCnt1);
+
+    for (uint32_t i = 0; i < nodeCnt1; i++) {
+        ans[i] = maxTree2 + (treeData1.nodeList[i] == NodeType::EVEN ? treeData1.evenCnt : treeData1.oddCnt);
+    }
+
+    return ans;
+}
