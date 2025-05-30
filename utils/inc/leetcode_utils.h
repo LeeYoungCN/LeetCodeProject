b13a1f0b78dfdb7d5de6d37cbf8f5dc64c26d153
ly <1175529741@qq.com>
2025-05-30 21:14:03 +0800

    utils

--- utils/inc/leetcode_utils.h
+++ utils/inc/leetcode_utils.h
@@ -1,13 +1,52 @@
-#ifndef GRAPH_H
-#define GRAPH_H
+#ifndef LEETCODE_UTILS_H
+#define LEETCODE_UTILS_H
 
 #include <cstdint>
+#include <iostream>
 #include <string>
 #include <vector>
 
-std::vector<std::vector<int32_t>> GetGraphByEdges(const std::vector<std::vector<int32_t>> &edges);
+std::vector<std::vector<uint32_t>> GetGraphByEdges(const std::vector<std::vector<int32_t>> &edges);
 
-std::string Vector2String(const std::vector<int32_t> &vec, bool hasIdx = false);
-void PrintVector(const std::vector<int32_t>& vec, bool hasIdx);
+template <class NUMBER>
+std::string Vector2String(const std::vector<NUMBER> &vec, uint32_t start = 0, uint32_t end = 0, bool hasIdx = true)
+{
+    if (end == 0) {
+        end = (uint32_t)vec.size();
+    }
 
-#endif  // GRAPH_H
+    const uint32_t length = end - start;
+    std::string str = "{";
+
+    for (uint32_t i = 0; i < length; i++) {
+        uint32_t index = i + start;
+
+        if (hasIdx) {
+            str += "[" + std::to_string(index) + "]=";
+        }
+
+        str += std::to_string(vec[i]);
+        if (i < length - 1) {
+            str += ", ";
+        }
+    }
+    str += "}";
+    return str;
+}
+
+template <class NUMBER>
+void PrintVector(const std::vector<NUMBER> &vec, uint32_t start = 0, uint32_t end = 0, bool hasIdx = true)
+{
+    std::cout << Vector2String(vec, start, end, hasIdx) << std::endl;
+}
+
+class UtilsDegbug {
+public:
+    virtual std::string ToString() = 0;
+};
+
+void PrintVector(const std::vector<UtilsDegbug *> &vec, uint32_t start = 0, uint32_t end = 0, bool hasIdx = true);
+std::string Vector2String(const std::vector<UtilsDegbug *> &vec, uint32_t start = 0, uint32_t end = 0,
+                          bool hasIdx = true);
+
+#endif  // LEETCODE_UTILS_H
--- utils/src/leetcode_utils.cpp
+++ utils/src/leetcode_utils.cpp
@@ -1,23 +1,23 @@
 #include "leetcode_utils.h"
 
 #include <cstdint>
-#include <cstdio>
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-vector<vector<int32_t>> GetGraphByEdges(const vector<vector<int32_t>>& edges)
+vector<vector<uint32_t>> GetGraphByEdges(const vector<vector<int32_t>> &edges)
 {
     const uint32_t START_NODE = 0;
     const uint32_t END_NODE = 1;
 
     uint32_t nodeCnt = (uint32_t)edges.size() + 1;
-    vector<vector<int32_t>> graph(nodeCnt, vector<int32_t>());
+    vector<vector<uint32_t>> graph(nodeCnt, vector<uint32_t>());
 
-    for (const vector<int32_t>& edge : edges) {
-        int32_t start = edge[START_NODE];
-        int32_t end = edge[END_NODE];
+    for (const vector<int32_t> &edge : edges) {
+        uint32_t start = (uint32_t)edge[START_NODE];
+        uint32_t end = (uint32_t)edge[END_NODE];
 
         graph[start].push_back(end);
         graph[end].push_back(start);
@@ -26,27 +26,32 @@ vector<vector<int32_t>> GetGraphByEdges(const vector<vector<int32_t>>& edges)
     return graph;
 }
 
-string Vector2String(const vector<int32_t>& vec, bool hasIdx)
+void PrintVector(const std::vector<UtilsDegbug *> &vec, uint32_t start, uint32_t end, bool hasIdx)
 {
-    const uint32_t length = vec.size();
-    string str = "{";
+    cout << Vector2String(vec, start, end, hasIdx) << endl;
+}
+
+std::string Vector2String(const std::vector<UtilsDegbug *> &vec, uint32_t start, uint32_t end, bool hasIdx)
+
+{
+    if (end == 0) {
+        end = (uint32_t)vec.size();
+    }
+
+    const uint32_t length = end - start;
+    std::string str = "{";
+
     for (uint32_t i = 0; i < length; i++) {
-        char tmp[512] = {'\0'};
+        uint32_t index = i + start;
         if (hasIdx) {
-            sprintf(tmp, "[%u]=%d", i, vec[i]);
-        } else {
-            sprintf(tmp, "%d", vec[i]);
+            str += "[" + std::to_string(index) + "]=";
         }
-        str += tmp;
+
+        str += vec[i]->ToString();
         if (i < length - 1) {
             str += ", ";
         }
     }
-    str += "{";
+    str += "}";
     return str;
-}
-
-void PrintVector(const vector<int32_t>& vec, bool hasIdx)
-{
-    printf("%s\n", Vector2String(vec, hasIdx).c_str());
 }
