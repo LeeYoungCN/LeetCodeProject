2aa1e8ac34a629ff56a7229793d2c5b2ea36435a
ly <1175529741@qq.com>
2025-05-31 22:26:50 +0800

    lc1456

---
+++ leetcode/inc/lc1456_maximum_number_of_vowels_in_a_substring_of_given_length.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-05-31 22:04:33
+ * URL  : https://leetcode.cn/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description/
+ */
+#ifndef LC1456_MAXIMUM_NUMBER_OF_VOWELS_IN_A_SUBSTRING_OF_GIVEN_LENGTH_H
+#define LC1456_MAXIMUM_NUMBER_OF_VOWELS_IN_A_SUBSTRING_OF_GIVEN_LENGTH_H
+
+#include <string>
+
+class LC1456_MaximumNumberOfVowelsInASubstringOfGivenLength {
+public:
+    LC1456_MaximumNumberOfVowelsInASubstringOfGivenLength() = default;
+    virtual ~LC1456_MaximumNumberOfVowelsInASubstringOfGivenLength() = default;
+    int maxVowels(std::string s, int k);
+};
+
+#endif  // LC1456_MAXIMUM_NUMBER_OF_VOWELS_IN_A_SUBSTRING_OF_GIVEN_LENGTH_H
---
+++ leetcode/src/lc1456_maximum_number_of_vowels_in_a_substring_of_given_length.cpp
@@ -0,0 +1,43 @@
+/*
+ * Time : 2025-05-31 22:04:33
+ * URL  : https://leetcode.cn/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description/
+ */
+#include "lc1456_maximum_number_of_vowels_in_a_substring_of_given_length.h"
+
+#include <algorithm>
+#include <cstdint>
+#include <functional>
+#include <string>
+
+using namespace std;
+
+int LC1456_MaximumNumberOfVowelsInASubstringOfGivenLength::maxVowels(string s, int k)
+{
+    const uint32_t length = (uint32_t)s.size();
+
+    function<bool(char)> isVowels = [](char c) -> bool {
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    };
+
+    uint32_t start = 0;
+    uint32_t tmp = 0;
+    uint32_t ans = 0;
+    for (uint32_t i = 0; i < length; i++) {
+        char c = s.at(i);
+        uint32_t end = i + 1;
+        if (isVowels(c)) {
+            tmp++;
+        }
+        if (end - start > (uint32_t)k) {
+            if (isVowels(s.at(start))) {
+                tmp--;
+            }
+            start++;
+        }
+        ans = max(ans, tmp);
+        if (ans >= (uint32_t)k) {
+            return k;
+        }
+    }
+    return (int32_t)ans;
+}
