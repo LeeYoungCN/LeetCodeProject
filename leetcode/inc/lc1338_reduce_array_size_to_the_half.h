26806a7d8ffe958e3ef18542d6aea6b927cfda2f
ly <1175529741@qq.com>
2025-06-02 00:16:05 +0800

    lc1338

---
+++ leetcode/inc/lc1338_reduce_array_size_to_the_half.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-01 23:45:39
+ * URL  : https://leetcode.cn/problems/reduce-array-size-to-the-half/
+ */
+#ifndef LC1338_REDUCE_ARRAY_SIZE_TO_THE_HALF_H
+#define LC1338_REDUCE_ARRAY_SIZE_TO_THE_HALF_H
+
+#include <vector>
+
+class LC1338_ReduceArraySizeToTheHalf {
+public:
+    LC1338_ReduceArraySizeToTheHalf()          = default;
+    virtual ~LC1338_ReduceArraySizeToTheHalf() = default;
+    int minSetSize(std::vector<int>& arr);
+};
+
+#endif  // LC1338_REDUCE_ARRAY_SIZE_TO_THE_HALF_H
---
+++ leetcode/src/lc1338_reduce_array_size_to_the_half.cpp
@@ -0,0 +1,40 @@
+/*
+ * Time : 2025-06-01 23:45:39
+ * URL  : https://leetcode.cn/problems/reduce-array-size-to-the-half/
+ */
+#include "lc1338_reduce_array_size_to_the_half.h"
+
+#include <algorithm>
+#include <cstdint>
+#include <functional>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int LC1338_ReduceArraySizeToTheHalf::minSetSize(std::vector<int>& arr)
+{
+    unordered_map<int32_t, int32_t> numMap;
+    for (int32_t n : arr) {
+        numMap[n]++;
+    }
+    uint32_t numTypeCnt = (uint32_t)numMap.size();
+    vector<int32_t> numCntVec;
+    numCntVec.reserve(numTypeCnt);
+
+    for (const auto& [num, cnt] : numMap) {
+        numCntVec.push_back(cnt);
+    }
+
+    sort(numCntVec.begin(), numCntVec.end(), greater<int32_t>());
+
+    int32_t half = (int32_t)arr.size() / 2;
+
+    for (uint32_t i = 0; i < numTypeCnt; i++) {
+        half -= numCntVec[i];
+        if (half <= 0) {
+            return (int32_t)i + 1;
+        }
+    }
+    return (int32_t)numTypeCnt;
+}
