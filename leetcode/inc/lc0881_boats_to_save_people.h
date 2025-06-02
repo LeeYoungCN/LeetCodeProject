147c52c3844b18a7f06d2714eb77b8736bc4e0ad
ly <1175529741@qq.com>
2025-06-02 16:53:35 +0800

    lc0881

---
+++ leetcode/inc/lc0881_boats_to_save_people.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-02 15:42:25
+ * URL  : https://leetcode.cn/problems/boats-to-save-people/description/
+ */
+#ifndef LC0881_BOATS_TO_SAVE_PEOPLE_H
+#define LC0881_BOATS_TO_SAVE_PEOPLE_H
+
+#include <vector>
+
+class LC0881_BoatsToSavePeople {
+public:
+    LC0881_BoatsToSavePeople() = default;
+    virtual ~LC0881_BoatsToSavePeople() = default;
+    int numRescueBoats(std::vector<int>& people, int limit);
+};
+
+#endif  // LC0881_BOATS_TO_SAVE_PEOPLE_H
