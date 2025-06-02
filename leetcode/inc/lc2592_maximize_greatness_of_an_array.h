a978799a7e423591177b7893cadecb5b146db47a
ly <1175529741@qq.com>
2025-06-02 23:39:35 +0800

    lc2592

---
+++ leetcode/inc/lc2592_maximize_greatness_of_an_array.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-02 17:06:28
+ * URL  : https://leetcode.cn/problems/maximize-greatness-of-an-array/description/
+ */
+#ifndef LC2592_MAXIMIZE_GREATNESS_OF_AN_ARRAY_H
+#define LC2592_MAXIMIZE_GREATNESS_OF_AN_ARRAY_H
+
+#include <vector>
+
+class LC2592_MaximizeGreatnessOfAnArray {
+public:
+    LC2592_MaximizeGreatnessOfAnArray() = default;
+    virtual ~LC2592_MaximizeGreatnessOfAnArray() = default;
+    int maximizeGreatness(std::vector<int>& nums);
+};
+
+#endif  // LC2592_MAXIMIZE_GREATNESS_OF_AN_ARRAY_H
