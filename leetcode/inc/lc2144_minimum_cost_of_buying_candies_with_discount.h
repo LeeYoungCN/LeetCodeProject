19f2af36f6c56198fbe809b281d4805667aea127
ly <1175529741@qq.com>
2025-06-02 15:40:53 +0800

    lc2144

---
+++ leetcode/inc/lc2144_minimum_cost_of_buying_candies_with_discount.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-02 15:20:38
+ * URL  : https://leetcode.cn/problems/minimum-cost-of-buying-candies-with-discount/description/
+ */
+#ifndef LC2144_MINIMUM_COST_OF_BUYING_CANDIES_WITH_DISCOUNT_H
+#define LC2144_MINIMUM_COST_OF_BUYING_CANDIES_WITH_DISCOUNT_H
+
+#include <vector>
+
+class LC2144_MinimumCostOfBuyingCandiesWithDiscount {
+public:
+    LC2144_MinimumCostOfBuyingCandiesWithDiscount() = default;
+    virtual ~LC2144_MinimumCostOfBuyingCandiesWithDiscount() = default;
+    int minimumCost(std::vector<int>& cost);
+};
+
+#endif  // LC2144_MINIMUM_COST_OF_BUYING_CANDIES_WITH_DISCOUNT_H
