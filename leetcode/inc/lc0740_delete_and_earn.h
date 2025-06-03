498312b7404a663ed4d00788c8fc1d715029abbb
ly <1175529741@qq.com>
2025-06-03 17:34:11 +0800

    lc0740

---
+++ leetcode/inc/lc0740_delete_and_earn.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-03 14:12:55
+ * URL  : https://leetcode.cn/problems/delete-and-earn/description/
+ */
+#ifndef LC0740_DELETE_AND_EARN_H
+#define LC0740_DELETE_AND_EARN_H
+
+#include <vector>
+
+class LC0740_DeleteAndEarn {
+public:
+    LC0740_DeleteAndEarn() = default;
+    virtual ~LC0740_DeleteAndEarn() = default;
+    int deleteAndEarn(std::vector<int>& nums);
+};
+
+#endif  // LC0740_DELETE_AND_EARN_H
