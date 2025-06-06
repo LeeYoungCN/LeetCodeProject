840ac7771825860a763d861d4b764231b6483953
ly <1175529741@qq.com>
2025-06-06 10:55:19 +0800

    lc2434

---
+++ leetcode/inc/lc2434_using_a_robot_to_print_the_lexicographically_smallest_string.h
@@ -0,0 +1,18 @@
+/*
+ * Time : 2025-06-06 01:21:03
+ * URL  :
+ * https://leetcode.cn/problems/using-a-robot-to-print-the-lexicographically-smallest-string/description/?envType=daily-questionURL_STRenvId=2025-06-06
+ */
+#ifndef LC2434_USING_A_ROBOT_TO_PRINT_THE_LEXICOGRAPHICALLY_SMALLEST_STRING_H
+#define LC2434_USING_A_ROBOT_TO_PRINT_THE_LEXICOGRAPHICALLY_SMALLEST_STRING_H
+
+#include <string>
+
+class LC2434_UsingARobotToPrintTheLexicographicallySmallestString {
+public:
+    LC2434_UsingARobotToPrintTheLexicographicallySmallestString() = default;
+    virtual ~LC2434_UsingARobotToPrintTheLexicographicallySmallestString() = default;
+    std::string robotWithString(std::string s);
+};
+
+#endif  // LC2434_USING_A_ROBOT_TO_PRINT_THE_LEXICOGRAPHICALLY_SMALLEST_STRING_H
