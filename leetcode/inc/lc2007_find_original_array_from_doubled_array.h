6685b86a8b83aeae8d6bf7ffd84505f70552b37d
ly <1175529741@qq.com>
2025-06-06 17:32:28 +0800

    lc2007

---
+++ leetcode/inc/lc2007_find_original_array_from_doubled_array.h
@@ -0,0 +1,24 @@
+/*
+ * Time : 2025-06-06 15:36:48
+ * URL  : https://leetcode.cn/problems/find-original-array-from-doubled-array/description/
+ */
+#ifndef LC2007_FIND_ORIGINAL_ARRAY_FROM_DOUBLED_ARRAY_H
+#define LC2007_FIND_ORIGINAL_ARRAY_FROM_DOUBLED_ARRAY_H
+
+#include <vector>
+
+class LC2007_FindOriginalArrayFromDoubledArray {
+public:
+    LC2007_FindOriginalArrayFromDoubledArray() = default;
+    virtual ~LC2007_FindOriginalArrayFromDoubledArray() = default;
+    virtual std::vector<int> findOriginalArray(std::vector<int>& changed) = 0;
+};
+
+class LC2007_FindOriginalArrayFromDoubledArray_Map : public LC2007_FindOriginalArrayFromDoubledArray {
+public:
+    LC2007_FindOriginalArrayFromDoubledArray_Map() = default;
+    virtual ~LC2007_FindOriginalArrayFromDoubledArray_Map() override = default;
+    std::vector<int> findOriginalArray(std::vector<int>& changed) override;
+};
+
+#endif  // LC2007_FIND_ORIGINAL_ARRAY_FROM_DOUBLED_ARRAY_H
---
+++ leetcode/src/lc2007_find_original_array_from_doubled_array.cpp
@@ -0,0 +1,56 @@
+/*
+ * Time : 2025-06-06 15:36:48
+ * URL  : https://leetcode.cn/problems/find-original-array-from-doubled-array/description/
+ */
+#include "lc2007_find_original_array_from_doubled_array.h"
+
+#include <cstdint>
+#include <functional>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+vector<int32_t> LC2007_FindOriginalArrayFromDoubledArray_Map::findOriginalArray(vector<int32_t> &changed)
+{
+    const uint32_t length = (uint32_t)changed.size();
+    if (length % 2 != 0) {
+        return {};
+    }
+
+    map<int32_t, uint32_t, less<>> changeMap;
+    for (int32_t n : changed) {
+        changeMap[n]++;
+    }
+
+    vector<int32_t> ans;
+    ans.reserve(length / 2);
+
+    if (changeMap.find(0) != changeMap.end()) {
+        if (changeMap[0] % 2 != 0) {
+            return {};
+        }
+        ans.insert(ans.end(), changeMap[0] / 2, 0);
+        changeMap.erase(0);
+    }
+
+    for (const auto [num, cnt] : changeMap) {
+        if (cnt == 0) {
+            continue;
+        }
+
+        int32_t target = num * 2;
+        map<int32_t, uint32_t, less<>>::iterator it = changeMap.find(target);
+
+        if (it == changeMap.end() || it->second < cnt) {
+            return {};
+        }
+
+        ans.insert(ans.end(), cnt, num);
+        changeMap[target] -= cnt;
+        changeMap[num] = 0;
+    }
+
+    return ans;
+}
+
