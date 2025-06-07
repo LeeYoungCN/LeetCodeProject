9a93886a88a73ed870a770ede3a2796837212c5f
ly <1175529741@qq.com>
2025-06-08 02:56:06 +0800

    lc0386

---
+++ leetcode/inc/lc0386_lexicographical_numbers.h
@@ -0,0 +1,52 @@
+/*
+ * Time : 2025-06-08 00:54:01
+ * URL  :
+ * https://leetcode.cn/problems/lexicographical-numbers/description/?envType=daily-questionURL_STRenvId=2025-06-08
+ */
+#ifndef LC0386_LEXICOGRAPHICAL_NUMBERS_H
+#define LC0386_LEXICOGRAPHICAL_NUMBERS_H
+
+#include <vector>
+
+class LC0386_LexicographicalNumbers {
+public:
+    LC0386_LexicographicalNumbers() = default;
+    virtual ~LC0386_LexicographicalNumbers() = default;
+    virtual std::vector<int> lexicalOrder(int n) = 0;
+};
+
+class LC0386_LexicographicalNumbers_Str2Num : public LC0386_LexicographicalNumbers {
+public:
+    LC0386_LexicographicalNumbers_Str2Num() = default;
+    ~LC0386_LexicographicalNumbers_Str2Num() override = default;
+    std::vector<int> lexicalOrder(int n) override;
+};
+
+class LC0386_LexicographicalNumbers_DFS : public LC0386_LexicographicalNumbers {
+public:
+    LC0386_LexicographicalNumbers_DFS() = default;
+    ~LC0386_LexicographicalNumbers_DFS() override = default;
+    std::vector<int> lexicalOrder(int n) override;
+};
+
+class NumIterator {
+public:
+    NumIterator() = default;
+    explicit NumIterator(int limit);
+    ~NumIterator() = default;
+    bool HasNext() const;
+    int Next();
+
+private:
+    int num = 1;
+    int limit;
+};
+
+class LC0386_LexicographicalNumbers_Iterator : public LC0386_LexicographicalNumbers {
+public:
+    LC0386_LexicographicalNumbers_Iterator() = default;
+    ~LC0386_LexicographicalNumbers_Iterator() override = default;
+    std::vector<int> lexicalOrder(int n) override;
+};
+
+#endif  // LC0386_LEXICOGRAPHICAL_NUMBERS_H
---
+++ leetcode/src/lc0386_lexicographical_numbers.cpp
@@ -0,0 +1,96 @@
+/*
+ * Time : 2025-06-08 00:54:01
+ * URL  :
+ * https://leetcode.cn/problems/lexicographical-numbers/description/?envType=daily-questionURL_STRenvId=2025-06-08
+ */
+#include "lc0386_lexicographical_numbers.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int32_t> LC0386_LexicographicalNumbers_Str2Num::lexicalOrder(int32_t n)
+{
+    set<string> strSet;
+    for (int32_t i = 1; i <= n; i++) {
+        strSet.insert(to_string(i));
+    }
+
+    vector<int32_t> ans;
+    ans.reserve((size_t)n);
+    for (string x : strSet) {
+        ans.push_back(atoi(x.c_str()));
+    }
+
+    return ans;
+}
+
+vector<int32_t> LC0386_LexicographicalNumbers_DFS::lexicalOrder(int32_t n)
+{
+    vector<int32_t> ans;
+    ans.reserve((size_t)n);
+
+    auto dfs = [&](auto &&dfs, int32_t base) -> bool {
+        if (base > n) {
+            return false;
+        }
+
+        if (base != 0) {
+            ans.push_back(base);
+        }
+
+        for (int32_t i = (base == 0 ? 1 : 0); i <= 9; ++i) {
+            if (!dfs(dfs, base * 10 + i)) {
+                break;
+            }
+        }
+        return true;
+    };
+
+    dfs(dfs, 0);
+
+    return ans;
+}
+
+NumIterator::NumIterator(int limit) : limit(limit) {}
+
+bool NumIterator::HasNext() const
+{
+    return num > 0;
+}
+
+int32_t NumIterator::Next()
+{
+    int32_t ret = num;
+
+    if (num * 10 <= limit) {
+        num *= 10;
+    } else {
+        while (num % 10 == 9 || num + 1 > limit) {
+            num /= 10;
+        }
+        if (num != 0) {
+            num++;
+        }
+    }
+
+    return ret;
+}
+
+vector<int32_t> LC0386_LexicographicalNumbers_Iterator::lexicalOrder(int32_t n)
+{
+    NumIterator iter(n);
+
+    vector<int32_t> ans;
+    ans.reserve((size_t)n);
+
+    while (iter.HasNext()) {
+        ans.push_back(iter.Next());
+    }
+
+    return ans;
+}
