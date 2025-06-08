6a5f62bdee5b45056c696e51648a69bfd034b149
ly <1175529741@qq.com>
2025-06-09 02:59:26 +0800

    lc0440

---
+++ leetcode/inc/lc0440_k_th_smallest_in_lexicographical_order.h
@@ -0,0 +1,16 @@
+/*
+ * Time : 2025-06-09 00:44:38
+ * URL  :
+ * https://leetcode.cn/problems/k-th-smallest-in-lexicographical-order/description/?envType=daily-questionURL_STRenvId=2025-06-09
+ */
+#ifndef LC0440_K_TH_SMALLEST_IN_LEXICOGRAPHICAL_ORDER_H
+#define LC0440_K_TH_SMALLEST_IN_LEXICOGRAPHICAL_ORDER_H
+
+class LC0440_KThSmallestInLexicographicalOrder {
+public:
+    LC0440_KThSmallestInLexicographicalOrder() = default;
+    virtual ~LC0440_KThSmallestInLexicographicalOrder() = default;
+    int findKthNumber(int n, int k);
+};
+
+#endif  // LC0440_K_TH_SMALLEST_IN_LEXICOGRAPHICAL_ORDER_H
