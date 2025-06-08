5583a1c926cdda405a939c8a9410e26ec90bd19f
ly <1175529741@qq.com>
2025-06-08 17:10:26 +0800

    lc3186

---
+++ leetcode/inc/lc3186_maximum_total_damage_with_spell_casting.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-02 23:56:29
+ * URL  : https://leetcode.cn/problems/maximum-total-damage-with-spell-casting/description/
+ */
+#ifndef LC3186_MAXIMUM_TOTAL_DAMAGE_WITH_SPELL_CASTING_H
+#define LC3186_MAXIMUM_TOTAL_DAMAGE_WITH_SPELL_CASTING_H
+
+#include <vector>
+
+class LC3186_MaximumTotalDamageWithSpellCasting {
+public:
+    LC3186_MaximumTotalDamageWithSpellCasting() = default;
+    virtual ~LC3186_MaximumTotalDamageWithSpellCasting() = default;
+    long long maximumTotalDamage(std::vector<int>& power);
+};
+
+#endif  // LC3186_MAXIMUM_TOTAL_DAMAGE_WITH_SPELL_CASTING_H
