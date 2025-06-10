4540bbb6cf6276c36be9d7c3438769d11e695712
ly <1175529741@qq.com>
2025-06-10 10:19:55 +0800

    lc3442

---
+++ leetcode/inc/lc3442_maximum_difference_between_even_and_odd_frequency_i.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-10 09:25:33
+ * URL  : https://leetcode.cn/problems/maximum-difference-between-even-and-odd-frequency-i/description/?envType=daily-questionURL_STRenvId=2025-06-10
+ */
+#ifndef LC3442_MAXIMUM_DIFFERENCE_BETWEEN_EVEN_AND_ODD_FREQUENCY_I_H
+#define LC3442_MAXIMUM_DIFFERENCE_BETWEEN_EVEN_AND_ODD_FREQUENCY_I_H
+
+#include <string>
+
+class LC3442_MaximumDifferenceBetweenEvenAndOddFrequencyI {
+public:
+    LC3442_MaximumDifferenceBetweenEvenAndOddFrequencyI() = default;
+    virtual ~LC3442_MaximumDifferenceBetweenEvenAndOddFrequencyI() = default;
+    int maxDifference(std::string s);
+};
+
+#endif  // LC3442_MAXIMUM_DIFFERENCE_BETWEEN_EVEN_AND_ODD_FREQUENCY_I_H
