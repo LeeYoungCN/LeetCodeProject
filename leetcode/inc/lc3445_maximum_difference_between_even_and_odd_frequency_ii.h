10de66f35dff6570e59843e00e89454aa217994e
LY_WSL <1175529741@qq.com>
2025-06-11 19:08:22 +0800

    lc3445

---
+++ leetcode/inc/lc3445_maximum_difference_between_even_and_odd_frequency_ii.h
@@ -0,0 +1,18 @@
+/*
+ * Time : 2025-06-11 09:53:03
+ * URL  :
+ * https://leetcode.cn/problems/maximum-difference-between-even-and-odd-frequency-ii/?envType=daily-questionURL_STRenvId=2025-06-11
+ */
+#ifndef LC3445_MAXIMUM_DIFFERENCE_BETWEEN_EVEN_AND_ODD_FREQUENCY_II_H
+#define LC3445_MAXIMUM_DIFFERENCE_BETWEEN_EVEN_AND_ODD_FREQUENCY_II_H
+
+#include <string>
+
+class LC3445_MaximumDifferenceBetweenEvenAndOddFrequencyII {
+public:
+    LC3445_MaximumDifferenceBetweenEvenAndOddFrequencyII() = default;
+    virtual ~LC3445_MaximumDifferenceBetweenEvenAndOddFrequencyII() = default;
+    int maxDifference(std::string s, int k);
+};
+
+#endif  // LC3445_MAXIMUM_DIFFERENCE_BETWEEN_EVEN_AND_ODD_FREQUENCY_II_H
