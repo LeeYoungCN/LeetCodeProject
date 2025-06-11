f4e5d9b321fa6075a890396fc021c2f60776ec2b
LY_WSL <1175529741@qq.com>
2025-06-11 14:32:00 +0800

    lc1371

---
+++ leetcode/inc/lc1371_find_the_longest_substring_containing_vowels_in_even_counts.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-11 13:21:38
+ * URL  : https://leetcode.cn/problems/find-the-longest-substring-containing-vowels-in-even-counts/description/
+ */
+#ifndef LC1371_FIND_THE_LONGEST_SUBSTRING_CONTAINING_VOWELS_IN_EVEN_COUNTS_H
+#define LC1371_FIND_THE_LONGEST_SUBSTRING_CONTAINING_VOWELS_IN_EVEN_COUNTS_H
+
+#include <string>
+
+class LC1371_FindTheLongestSubstringContainingVowelsInEvenCounts {
+public:
+    LC1371_FindTheLongestSubstringContainingVowelsInEvenCounts() = default;
+    virtual ~LC1371_FindTheLongestSubstringContainingVowelsInEvenCounts() = default;
+    int findTheLongestSubstring(std::string s);
+};
+
+#endif  // LC1371_FIND_THE_LONGEST_SUBSTRING_CONTAINING_VOWELS_IN_EVEN_COUNTS_H
