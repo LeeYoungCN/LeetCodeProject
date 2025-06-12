bc53b9827b0318813d3f260965363afa5ea869d0
LY_WSL <1175529741@qq.com>
2025-06-12 18:58:41 +0800

    lc0560

---
+++ leetcode/inc/lc0560_subarray_sum_equals_k.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-12 18:03:55
+ * URL  : https://leetcode.cn/problems/subarray-sum-equals-k/description/?envType=problem-list-v2URL_STRenvId=prefix-sum
+ */
+#ifndef LC0560_SUBARRAY_SUM_EQUALS_K_H
+#define LC0560_SUBARRAY_SUM_EQUALS_K_H
+
+#include <vector>
+
+class LC0560_SubarraySumEqualsK {
+public:
+    LC0560_SubarraySumEqualsK() = default;
+    virtual ~LC0560_SubarraySumEqualsK() = default;
+    int subarraySum(std::vector<int>& nums, int k);
+};
+
+#endif  // LC0560_SUBARRAY_SUM_EQUALS_K_H
