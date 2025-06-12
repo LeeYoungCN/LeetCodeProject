6b55ee5aa53df31f918a672ba3bb5116109f9399
LY_WSL <1175529741@qq.com>
2025-06-12 10:11:16 +0800

    lc3423

---
+++ leetcode/inc/lc3423_maximum_difference_between_adjacent_elements_in_a_circular_array.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-12 10:00:49
+ * URL  : https://leetcode.cn/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/?envType=daily-questionURL_STRenvId=2025-06-12
+ */
+#ifndef LC3423_MAXIMUM_DIFFERENCE_BETWEEN_ADJACENT_ELEMENTS_IN_A_CIRCULAR_ARRAY_H
+#define LC3423_MAXIMUM_DIFFERENCE_BETWEEN_ADJACENT_ELEMENTS_IN_A_CIRCULAR_ARRAY_H
+
+#include <vector>
+
+class LC3423_MaximumDifferenceBetweenAdjacentElementsInACircularArray {
+public:
+    LC3423_MaximumDifferenceBetweenAdjacentElementsInACircularArray() = default;
+    virtual ~LC3423_MaximumDifferenceBetweenAdjacentElementsInACircularArray() = default;
+    int maxAdjacentDistance(std::vector<int>& nums);
+};
+
+#endif  // LC3423_MAXIMUM_DIFFERENCE_BETWEEN_ADJACENT_ELEMENTS_IN_A_CIRCULAR_ARRAY_H
