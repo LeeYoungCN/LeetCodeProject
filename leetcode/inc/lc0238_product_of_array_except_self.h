0b10819301651707e4d8f8b474d1df6654d5be83
LY_WSL <1175529741@qq.com>
2025-06-12 15:48:51 +0800

    lc0238

---
+++ leetcode/inc/lc0238_product_of_array_except_self.h
@@ -0,0 +1,18 @@
+/*
+ * Time : 2025-06-12 14:21:47
+ * URL  :
+ * https://leetcode.cn/problems/product-of-array-except-self/description/?envType=problem-list-v2URL_STRenvId=prefix-sum
+ */
+#ifndef LC0238_PRODUCT_OF_ARRAY_EXCEPT_SELF_H
+#define LC0238_PRODUCT_OF_ARRAY_EXCEPT_SELF_H
+
+#include <vector>
+
+class LC0238_ProductOfArrayExceptSelf {
+public:
+    LC0238_ProductOfArrayExceptSelf() = default;
+    virtual ~LC0238_ProductOfArrayExceptSelf() = default;
+    std::vector<int> productExceptSelf(std::vector<int>& nums);
+};
+
+#endif  // LC0238_PRODUCT_OF_ARRAY_EXCEPT_SELF_H
---
+++ leetcode/src/lc0238_product_of_array_except_self.cpp
@@ -0,0 +1,31 @@
+/*
+ * Time : 2025-06-12 14:21:47
+ * URL  :
+ * https://leetcode.cn/problems/product-of-array-except-self/description/?envType=problem-list-v2URL_STRenvId=prefix-sum
+ */
+#include "lc0238_product_of_array_except_self.h"
+
+#include <cstdint>
+#include <vector>
+
+using namespace std;
+
+vector<int> LC0238_ProductOfArrayExceptSelf::productExceptSelf(vector<int>& nums)
+{
+    const uint32_t length = (uint32_t)nums.size();
+    vector<int32_t> preifxSum(length, 1);
+    vector<int32_t> suffixSum(length, 1);
+
+    for (uint32_t i = 1; i < length; i++) {
+        preifxSum[i] = preifxSum[i - 1] * nums[i - 1];
+        uint32_t j = length - i - 1;
+        suffixSum[j] = suffixSum[j + 1] * nums[j + 1];
+    }
+
+    vector<int32_t> ans(length, 0);
+    for (uint32_t i = 0; i < length; i++) {
+        ans[i] = preifxSum[i] * suffixSum[i];
+    }
+
+    return ans;
+}
