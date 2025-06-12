bf82b551238ab84585557dd53aa0f78871633eb3
LY_WSL <1175529741@qq.com>
2025-06-12 14:17:24 +0800

    lc0209

---
+++ leetcode/inc/lc0209_minimum_size_subarray_sum.h
@@ -0,0 +1,49 @@
+/*
+ * Time : 2025-06-12 10:12:40
+ * URL  :
+ * https://leetcode.cn/problems/minimum-size-subarray-sum/description/?envType=problem-list-v2URL_STRenvId=prefix-sum
+ */
+#ifndef LC0209_MINIMUM_SIZE_SUBARRAY_SUM_H
+#define LC0209_MINIMUM_SIZE_SUBARRAY_SUM_H
+
+#include <vector>
+
+class LC0209_MinimumSizeSubarraySum {
+public:
+    LC0209_MinimumSizeSubarraySum() = default;
+    virtual ~LC0209_MinimumSizeSubarraySum() = default;
+    virtual int minSubArrayLen(int target, std::vector<int>& nums) = 0;
+};
+
+class LC0209_MinimumSizeSubarraySum_MyLowerBound : public LC0209_MinimumSizeSubarraySum {
+public:
+    LC0209_MinimumSizeSubarraySum_MyLowerBound() = default;
+    ~LC0209_MinimumSizeSubarraySum_MyLowerBound() override = default;
+    int minSubArrayLen(int target, std::vector<int>& nums) override;
+
+private:
+    unsigned int FindLastLessEqual(const std::vector<int> prefixSums, int num);
+};
+
+class LC0209_MinimumSizeSubarraySum_StdLowerBound : public LC0209_MinimumSizeSubarraySum {
+public:
+    LC0209_MinimumSizeSubarraySum_StdLowerBound() = default;
+    virtual ~LC0209_MinimumSizeSubarraySum_StdLowerBound() override = default;
+    int minSubArrayLen(int target, std::vector<int>& nums) override;
+};
+
+class LC0209_MinimumSizeSubarraySum_Loop : public LC0209_MinimumSizeSubarraySum {
+public:
+    LC0209_MinimumSizeSubarraySum_Loop() = default;
+    virtual ~LC0209_MinimumSizeSubarraySum_Loop() override = default;
+    int minSubArrayLen(int target, std::vector<int>& nums) override;
+};
+
+class LC0209_MinimumSizeSubarraySum_SlidingWindow : public LC0209_MinimumSizeSubarraySum {
+public:
+    LC0209_MinimumSizeSubarraySum_SlidingWindow() = default;
+    virtual ~LC0209_MinimumSizeSubarraySum_SlidingWindow() override = default;
+    int minSubArrayLen(int target, std::vector<int>& nums) override;
+};
+
+#endif  // LC0209_MINIMUM_SIZE_SUBARRAY_SUM_H
