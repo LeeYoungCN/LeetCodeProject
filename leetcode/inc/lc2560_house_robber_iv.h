5f6d588ae38b82e2250abf4e71babc301eef66c7
LY_WSL <1175529741@qq.com>
2025-06-13 21:23:03 +0800

    lc2560

---
+++ leetcode/inc/lc2560_house_robber_iv.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-13 17:39:36
+ * URL  : https://leetcode.cn/problems/house-robber-iv/description/
+ */
+#ifndef LC2560_HOUSE_ROBBER_IV_H
+#define LC2560_HOUSE_ROBBER_IV_H
+
+#include <vector>
+
+class LC2560_HouseRobberIV {
+public:
+    LC2560_HouseRobberIV() = default;
+    virtual ~LC2560_HouseRobberIV() = default;
+    int minCapability(std::vector<int>& nums, int k);
+};
+
+#endif  // LC2560_HOUSE_ROBBER_IV_H
