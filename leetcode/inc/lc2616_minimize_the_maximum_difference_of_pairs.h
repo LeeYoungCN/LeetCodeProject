d9ad7360f9da825be92d9149de911ade80976c85
LY_WSL <1175529741@qq.com>
2025-06-13 17:24:08 +0800

    lc2616

---
+++ leetcode/inc/lc2616_minimize_the_maximum_difference_of_pairs.h
@@ -0,0 +1,18 @@
+/*
+ * Time : 2025-06-13 14:04:58
+ * URL  :
+ * https://leetcode.cn/problems/minimize-the-maximum-difference-of-pairs/description/?envType=daily-questionURL_STRenvId=2025-06-13
+ */
+#ifndef LC2616_MINIMIZE_THE_MAXIMUM_DIFFERENCE_OF_PAIRS_H
+#define LC2616_MINIMIZE_THE_MAXIMUM_DIFFERENCE_OF_PAIRS_H
+
+#include <vector>
+
+class LC2616_MinimizeTheMaximumDifferenceOfPairs {
+public:
+    LC2616_MinimizeTheMaximumDifferenceOfPairs() = default;
+    virtual ~LC2616_MinimizeTheMaximumDifferenceOfPairs() = default;
+    int minimizeMax(std::vector<int>& nums, int p);
+};
+
+#endif  // LC2616_MINIMIZE_THE_MAXIMUM_DIFFERENCE_OF_PAIRS_H
