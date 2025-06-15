647828fdc18ffa402c32863722ccbdd60380af97
LY_WSL <1175529741@qq.com>
2025-06-15 22:55:12 +0800

    lc1432

---
+++ leetcode/inc/lc1432_max_difference_you_can_get_from_changing_an_integer.h
@@ -0,0 +1,15 @@
+/*
+ * Time : 2025-06-15 19:50:29
+ * URL  : https://leetcode.cn/problems/max-difference-you-can-get-from-changing-an-integer/description/?envType=daily-questionURL_STRenvId=2025-06-15
+ */
+#ifndef LC1432_MAX_DIFFERENCE_YOU_CAN_GET_FROM_CHANGING_AN_INTEGER_H
+#define LC1432_MAX_DIFFERENCE_YOU_CAN_GET_FROM_CHANGING_AN_INTEGER_H
+
+class LC1432_MaxDifferenceYouCanGetFromChangingAnInteger {
+public:
+    LC1432_MaxDifferenceYouCanGetFromChangingAnInteger() = default;
+    virtual ~LC1432_MaxDifferenceYouCanGetFromChangingAnInteger() = default;
+    int maxDiff(int num);
+};
+
+#endif  // LC1432_MAX_DIFFERENCE_YOU_CAN_GET_FROM_CHANGING_AN_INTEGER_H
