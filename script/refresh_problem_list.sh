3c99b7e0098785e501cacd6fd4cdedbcd05bbebd
LY_WSL <1175529741@qq.com>
2025-06-16 12:51:23 +0800

    problem list file

---
+++ script/refresh_problem_list.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+SCRIPT_DIR="$(
+    cd "$(dirname "$0")" || exit 1
+    pwd
+)"
+ROOT_DIR="$(
+    cd "${SCRIPT_DIR}/.." || exit 1
+    pwd
+)"
+
+readonly SCRIPT_DIR
+readonly ROOT_DIR
+
+# shellcheck disable=SC1091
+source "${SCRIPT_DIR}/common_func.sh"
+
+readonly LEETCODE_INC_DIR="${ROOT_DIR}/leetcode/inc"
+readonly LEETCODE_SRC_DIR="${ROOT_DIR}/leetcode/src"
+readonly TEST_DIR="${ROOT_DIR}/test"
+readonly PROBLEM_LIST_FILE="${ROOT_DIR}/problem_list.md"
+
+if [ -e "${PROBLEM_LIST_FILE}" ]; then
+    rm "${PROBLEM_LIST_FILE}"
+fi
+
+find "${LEETCODE_SRC_DIR}" -maxdepth 1 -type f -exec bash -c '
+  for file do
+    base=$(basename "${file}")
+    echo "${base%.*}"  # 去除最后一个扩展名
+  done
+' _ {} + | sort >"${PROBLEM_LIST_FILE}"
+
+print_log "Refresh ${PROBLEM_LIST_FILE} success." info
