a05ab902bc1c76225fa59d34c54ecf7218581346
LY_WSL <1175529741@qq.com>
2025-06-20 02:41:30 +0800

    lc3443

---
+++ leetcode/inc/lc3443_maximum_manhattan_distance_after_k_changes.h
@@ -0,0 +1,18 @@
+/*
+ * Time : 2025-06-20 00:29:16
+ * URL  :
+ * https://leetcode.cn/problems/maximum-manhattan-distance-after-k-changes/description/?envType=daily-questionURL_STRenvId=2025-06-20
+ */
+#ifndef LC3443_MAXIMUM_MANHATTAN_DISTANCE_AFTER_K_CHANGES_H
+#define LC3443_MAXIMUM_MANHATTAN_DISTANCE_AFTER_K_CHANGES_H
+
+#include <string>
+
+class LC3443_MaximumManhattanDistanceAfterKChanges {
+public:
+    LC3443_MaximumManhattanDistanceAfterKChanges() = default;
+    virtual ~LC3443_MaximumManhattanDistanceAfterKChanges() = default;
+    int maxDistance(std::string s, int k);
+};
+
+#endif  // LC3443_MAXIMUM_MANHATTAN_DISTANCE_AFTER_K_CHANGES_H
