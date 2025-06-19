2928e57f724c7cfebbaeb34a3e9a91ba72ed5e75
LY_WSL <1175529741@qq.com>
2025-06-20 00:27:34 +0800

    lc2294

---
+++ leetcode/inc/lc2294_partition_array_such_that_maximum_difference_is_k.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-20 00:23:10
+ * URL  : https://leetcode.cn/problems/partition-array-such-that-maximum-difference-is-k/description/
+ */
+#ifndef LC2294_PARTITION_ARRAY_SUCH_THAT_MAXIMUM_DIFFERENCE_IS_K_H
+#define LC2294_PARTITION_ARRAY_SUCH_THAT_MAXIMUM_DIFFERENCE_IS_K_H
+
+#include <vector>
+
+class LC2294_PartitionArraySuchThatMaximumDifferenceIsK {
+public:
+    LC2294_PartitionArraySuchThatMaximumDifferenceIsK() = default;
+    virtual ~LC2294_PartitionArraySuchThatMaximumDifferenceIsK() = default;
+    int partitionArray(std::vector<int>& nums, int k);
+};
+
+#endif  // LC2294_PARTITION_ARRAY_SUCH_THAT_MAXIMUM_DIFFERENCE_IS_K_H
