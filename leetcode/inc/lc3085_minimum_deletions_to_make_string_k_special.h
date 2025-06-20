bc74c800f61baacae49d6354773fe09f00efac7f
ly <1175529741@qq.com>
2025-06-21 00:51:23 +0800

    lc3085

---
+++ leetcode/inc/lc3085_minimum_deletions_to_make_string_k_special.h
@@ -0,0 +1,18 @@
+/*
+ * Time : 2025-06-21 00:09:29
+ * URL  :
+ * https://leetcode.cn/problems/minimum-deletions-to-make-string-k-special/description/?envType=daily-question&envId=2025-06-21
+ */
+#ifndef LC3085_MINIMUM_DELETIONS_TO_MAKE_STRING_K_SPECIAL_H
+#define LC3085_MINIMUM_DELETIONS_TO_MAKE_STRING_K_SPECIAL_H
+
+#include <string>
+
+class LC3085_MinimumDeletionsToMakeStringKSpecial {
+public:
+    LC3085_MinimumDeletionsToMakeStringKSpecial() = default;
+    virtual ~LC3085_MinimumDeletionsToMakeStringKSpecial() = default;
+    int minimumDeletions(std::string word, int k);
+};
+
+#endif  // LC3085_MINIMUM_DELETIONS_TO_MAKE_STRING_K_SPECIAL_H
