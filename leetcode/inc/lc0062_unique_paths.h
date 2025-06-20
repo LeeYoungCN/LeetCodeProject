eecf153f762a2f9972ecd9fb5a169c2829f1ffc4
ly <1175529741@qq.com>
2025-06-20 18:10:37 +0800

    lc0062

---
+++ leetcode/inc/lc0062_unique_paths.h
@@ -0,0 +1,15 @@
+/*
+ * Time : 2025-06-20 16:59:36
+ * URL  : https://leetcode.cn/problems/unique-paths/description/?envType=problem-list-v2&envId=dynamic-programming
+ */
+#ifndef LC0062_UNIQUE_PATHS_H
+#define LC0062_UNIQUE_PATHS_H
+
+class LC0062_UniquePaths {
+public:
+    LC0062_UniquePaths() = default;
+    virtual ~LC0062_UniquePaths() = default;
+    int uniquePaths(int m, int n);
+};
+
+#endif  // LC0062_UNIQUE_PATHS_H
