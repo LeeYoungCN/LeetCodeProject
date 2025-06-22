c9f73a843b1a298359d93bbbcdd3f04a32042c5a
ly <1175529741@qq.com>
2025-06-22 16:29:09 +0800

    clang msvc nija

--- cmake/toolchain_files/clang_msvc.cmake
+++ cmake/toolchain_files/clang_msvc.cmake
@@ -1,3 +1,3 @@
 set(CMAKE_C_COMPILER "D:/Programs/LLVM_msvc/bin/clang.exe")
 set(CMAKE_CXX_COMPILER "D:/Programs/LLVM_msvc/bin/clang++.exe")
-set(CMAKE_GENERATOR "MinGW Makefiles")
+set(CMAKE_GENERATOR "Ninja")
