502badee46d46a98a07f5fbcfa631dd903d8cc16
LY_WSL <1175529741@qq.com>
2025-06-22 19:44:19 +0800

    lc0091_decode_ways

---
+++ leetcode/inc/lc0091_decode_ways.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-22 18:18:39
+ * URL  : https://leetcode.cn/problems/decode-ways/description/?envType=problem-list-v2&envId=dynamic-programming
+ */
+#ifndef LC0091_DECODE_WAYS_H
+#define LC0091_DECODE_WAYS_H
+
+#include <string>
+
+class LC0091_DecodeWays {
+public:
+    LC0091_DecodeWays() = default;
+    virtual ~LC0091_DecodeWays() = default;
+    int numDecodings(std::string s);
+};
+
+#endif  // LC0091_DECODE_WAYS_H
