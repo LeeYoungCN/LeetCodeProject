0e61370fcf750f0fd9c41f942ee363f5809d7a56
ly <1175529741@qq.com>
2025-06-24 11:48:09 +0800

    lc0119_pascals_triangle_ii

---
+++ leetcode/inc/lc0119_pascals_triangle_ii.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-24 11:31:51
+ * URL  : https://leetcode.cn/problems/pascals-triangle-ii/?envType=problem-list-v2&envId=dynamic-programming
+ */
+#ifndef LC0119_PASCALS_TRIANGLE_II_H
+#define LC0119_PASCALS_TRIANGLE_II_H
+
+#include <vector>
+
+class LC0119_PascalsTriangleII {
+public:
+    LC0119_PascalsTriangleII() = default;
+    virtual ~LC0119_PascalsTriangleII() = default;
+    std::vector<int> getRow(int rowIndex);
+};
+
+#endif  // LC0119_PASCALS_TRIANGLE_II_H
