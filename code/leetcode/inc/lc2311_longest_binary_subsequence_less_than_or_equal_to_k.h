55176f36b9945f22496af6477fccb2d5e9876f04
ly <1175529741@qq.com>
2025-06-27 01:21:42 +0800

    lc2311_longest_binary_subsequence_less_than_or_equal_to_k

---
+++ code/leetcode/inc/lc2311_longest_binary_subsequence_less_than_or_equal_to_k.h
@@ -0,0 +1,18 @@
+/*
+ * Time : 2025-06-26 18:52:09
+ * URL  :
+ * https://leetcode.cn/problems/longest-binary-subsequence-less-than-or-equal-to-k/description/?envType=daily-question&envId=2025-06-26
+ */
+#ifndef LC2311_LONGEST_BINARY_SUBSEQUENCE_LESS_THAN_OR_EQUAL_TO_K_H
+#define LC2311_LONGEST_BINARY_SUBSEQUENCE_LESS_THAN_OR_EQUAL_TO_K_H
+
+#include <string>
+
+class LC2311_LongestBinarySubsequenceLessThanOrEqualToK {
+public:
+    LC2311_LongestBinarySubsequenceLessThanOrEqualToK() = default;
+    virtual ~LC2311_LongestBinarySubsequenceLessThanOrEqualToK() = default;
+    int longestSubsequence(std::string s, int k);
+};
+
+#endif  // LC2311_LONGEST_BINARY_SUBSEQUENCE_LESS_THAN_OR_EQUAL_TO_K_H
