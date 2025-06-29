738c40c2419e9c93b0ef5089234b3c328182b390
LY_WSL <1175529741@qq.com>
2025-06-29 16:08:01 +0800

    lc2824_count_pairs_whose_sum_is_less_than_target

---
+++ code/leetcode/inc/lc2824_count_pairs_whose_sum_is_less_than_target.h
@@ -0,0 +1,43 @@
+/*
+ * Time : 2025-06-29 14:38:01
+ * URL  : https://leetcode.cn/problems/count-pairs-whose-sum-is-less-than-target/description/
+ */
+#ifndef LC2824_COUNT_PAIRS_WHOSE_SUM_IS_LESS_THAN_TARGET_H
+#define LC2824_COUNT_PAIRS_WHOSE_SUM_IS_LESS_THAN_TARGET_H
+
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+class LC2824_CountPairsWhoseSumIsLessThanTarget {
+public:
+    LC2824_CountPairsWhoseSumIsLessThanTarget() = default;
+    virtual ~LC2824_CountPairsWhoseSumIsLessThanTarget() = default;
+    virtual int32_t countPairs(std::vector<int32_t>& nums, int32_t target) = 0;
+};
+
+class LC2824_CountPairsWhoseSumIsLessThanTarget_Enum : public LC2824_CountPairsWhoseSumIsLessThanTarget {
+public:
+    LC2824_CountPairsWhoseSumIsLessThanTarget_Enum() = default;
+    ~LC2824_CountPairsWhoseSumIsLessThanTarget_Enum() override = default;
+    int32_t countPairs(std::vector<int32_t>& nums, int32_t target) override;
+};
+
+class LC2824_CountPairsWhoseSumIsLessThanTarget_Find : public LC2824_CountPairsWhoseSumIsLessThanTarget {
+public:
+    LC2824_CountPairsWhoseSumIsLessThanTarget_Find() = default;
+    ~LC2824_CountPairsWhoseSumIsLessThanTarget_Find() override = default;
+    int32_t countPairs(std::vector<int32_t>& nums, int32_t target) override;
+
+private:
+    size_t findLowerBound(const std::vector<int32_t>& nums, size_t end, int32_t target);
+};
+
+class LC2824_CountPairsWhoseSumIsLessThanTarget_DoublePtr : public LC2824_CountPairsWhoseSumIsLessThanTarget {
+    public:
+    LC2824_CountPairsWhoseSumIsLessThanTarget_DoublePtr() = default;
+    ~LC2824_CountPairsWhoseSumIsLessThanTarget_DoublePtr() override = default;
+    int32_t countPairs(std::vector<int32_t>& nums, int32_t target) override;
+};
+
+#endif  // LC2824_COUNT_PAIRS_WHOSE_SUM_IS_LESS_THAN_TARGET_H
---
+++ code/leetcode/src/lc2824_count_pairs_whose_sum_is_less_than_target.cpp
@@ -0,0 +1,73 @@
+/*
+ * Time : 2025-06-29 14:38:01
+ * URL  : https://leetcode.cn/problems/count-pairs-whose-sum-is-less-than-target/description/
+ */
+#include "lc2824_count_pairs_whose_sum_is_less_than_target.h"
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+using namespace std;
+
+int32_t LC2824_CountPairsWhoseSumIsLessThanTarget_Enum::countPairs(std::vector<int32_t>& nums, int32_t target)
+{
+    int32_t ans = 0;
+    for (uint32_t i = 0; i < nums.size() - 1; i++) {
+        for (uint32_t j = i + 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] < target) {
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
+
+size_t LC2824_CountPairsWhoseSumIsLessThanTarget_Find::findLowerBound(const std::vector<int32_t>& nums, size_t end,
+                                                                      int32_t target)
+{
+    size_t left = 0;
+    size_t right = end;
+    size_t ans = end;
+    while (left < right) {
+        size_t curr = (left + right) / 2;
+        if (nums[curr] < target) {
+            left = curr + 1;
+            ans = curr;
+        } else {
+            right = curr;
+        }
+    }
+    return ans;
+}
+
+int32_t LC2824_CountPairsWhoseSumIsLessThanTarget_Find::countPairs(std::vector<int32_t>& nums, int32_t target)
+{
+    ranges::sort(nums);
+
+    size_t ans = 0;
+    for (size_t i = 0; i < nums.size(); i++) {
+        size_t lowerBound = findLowerBound(nums, i, target - nums[i]);
+        if (lowerBound < i) {
+            ans += lowerBound + 1;
+        }
+    }
+
+    return static_cast<int32_t>(ans);
+}
+
+int32_t LC2824_CountPairsWhoseSumIsLessThanTarget_DoublePtr::countPairs(std::vector<int32_t>& nums, int32_t target)
+{
+    ranges::sort(nums);
+    uint32_t ans = 0;
+    for (size_t left = 0, right = nums.size() - 1; left < right; left++) {
+        while (nums[left] + nums[right] >= target && left < right) {
+            right--;
+        }
+        if (left < right) {
+            ans += right - left;
+        }
+    }
+    return static_cast<int32_t>(ans);
+}
