11327697fb0425a605cbd5facfcc7dfef21883e0
ly <1175529741@qq.com>
2025-06-30 00:33:09 +0800

    lc0594_longest_harmonious_subsequence

---
+++ code/leetcode/inc/lc0594_longest_harmonious_subsequence.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-06-30 00:24:59
+ * URL  : https://leetcode.cn/problems/longest-harmonious-subsequence/?envType=daily-question&envId=2025-06-30
+ */
+#ifndef LC0594_LONGEST_HARMONIOUS_SUBSEQUENCE_H
+#define LC0594_LONGEST_HARMONIOUS_SUBSEQUENCE_H
+
+#include <vector>
+
+class LC0594_LongestHarmoniousSubsequence {
+public:
+    LC0594_LongestHarmoniousSubsequence() = default;
+    virtual ~LC0594_LongestHarmoniousSubsequence() = default;
+    int32_t findLHS(std::vector<int32_t>& nums);
+};
+
+#endif  // LC0594_LONGEST_HARMONIOUS_SUBSEQUENCE_H
