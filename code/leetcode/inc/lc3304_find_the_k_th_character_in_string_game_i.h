bcfbca66fcadfb3900e0947dfe39d0f7963b7916
ly <1175529741@qq.com>
2025-07-03 15:15:49 +0800

    lc3304_find_the_k_th_character_in_string_game_i

---
+++ code/leetcode/inc/lc3304_find_the_k_th_character_in_string_game_i.h
@@ -0,0 +1,18 @@
+/*
+ * Time : 2025-07-03 14:46:38
+ * URL  :
+ * https://leetcode.cn/problems/find-the-k-th-character-in-string-game-i/description/?envType=daily-question&envId=2025-07-03
+ */
+#ifndef LC3304_FIND_THE_K_TH_CHARACTER_IN_STRING_GAME_I_H
+#define LC3304_FIND_THE_K_TH_CHARACTER_IN_STRING_GAME_I_H
+
+#include <cstdint>
+
+class LC3304_FindTheKThCharacterInStringGameI {
+public:
+    LC3304_FindTheKThCharacterInStringGameI() = default;
+    virtual ~LC3304_FindTheKThCharacterInStringGameI() = default;
+    char kthCharacter(int32_t k);
+};
+
+#endif  // LC3304_FIND_THE_K_TH_CHARACTER_IN_STRING_GAME_I_H
