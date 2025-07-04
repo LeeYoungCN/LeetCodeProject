1046cc6105b01dd008882d9321e6dd523e147a00
LY_WSL <1175529741@qq.com>
2025-07-04 15:49:44 +0800

    lc3307_find_the_k_th_character_in_string_game_ii

---
+++ code/leetcode/inc/lc3307_find_the_k_th_character_in_string_game_ii.h
@@ -0,0 +1,17 @@
+/*
+ * Time : 2025-07-03 15:31:31
+ * URL  : https://leetcode.cn/problems/find-the-k-th-character-in-string-game-ii/description/
+ */
+#ifndef LC3307_FIND_THE_K_TH_CHARACTER_IN_STRING_GAME_II_H
+#define LC3307_FIND_THE_K_TH_CHARACTER_IN_STRING_GAME_II_H
+
+#include <vector>
+
+class LC3307_FindTheKThCharacterInStringGameII {
+public:
+    LC3307_FindTheKThCharacterInStringGameII() = default;
+    virtual ~LC3307_FindTheKThCharacterInStringGameII() = default;
+    char kthCharacter(long long k, std::vector<int>& operations);
+};
+
+#endif  // LC3307_FIND_THE_K_TH_CHARACTER_IN_STRING_GAME_II_H
