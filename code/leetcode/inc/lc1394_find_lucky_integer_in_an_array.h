9e1322e5ba6ee395060efdfc17b34b3e292f8f06
ly <1175529741@qq.com>
2025-07-05 10:54:24 +0800

    lc1394_find_lucky_integer_in_an_array

---
+++ code/leetcode/inc/lc1394_find_lucky_integer_in_an_array.h
@@ -0,0 +1,19 @@
+/*
+ * Time : 2025-07-05 10:37:30
+ * URL  :
+ * https://leetcode.cn/problems/find-lucky-integer-in-an-array/description/?envType=daily-question&envId=2025-07-05
+ */
+#ifndef LC1394_FIND_LUCKY_INTEGER_IN_AN_ARRAY_H
+#define LC1394_FIND_LUCKY_INTEGER_IN_AN_ARRAY_H
+
+#include <cstdint>
+#include <vector>
+
+class LC1394_FindLuckyIntegerInAnArray {
+public:
+    LC1394_FindLuckyIntegerInAnArray() = default;
+    virtual ~LC1394_FindLuckyIntegerInAnArray() = default;
+    int32_t findLucky(std::vector<int32_t>& arr);
+};
+
+#endif  // LC1394_FIND_LUCKY_INTEGER_IN_AN_ARRAY_H
