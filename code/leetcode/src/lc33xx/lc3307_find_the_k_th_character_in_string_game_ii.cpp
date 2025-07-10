7a5168c16f5868eab4a347fbac37226f97d918ce
LY_WSL <1175529741@qq.com>
2025-07-10 09:39:34 +0800

    fix warnning

--- code/leetcode/src/lc33xx/lc3307_find_the_k_th_character_in_string_game_ii.cpp
+++ code/leetcode/src/lc33xx/lc3307_find_the_k_th_character_in_string_game_ii.cpp
@@ -14,10 +14,10 @@ char LC3307_FindTheKThCharacterInStringGameII::kthCharacter(long long k, std::ve
 {
     int32_t optCnt = 0;
 
-    for (size_t idx = operations.size() - 1; idx >= 0 && idx < operations.size(); idx--) {
+    for (size_t idx = operations.size(); idx > 0; idx--) {
         if (idx < 63 && k > (1LL << idx)) {
-            k -= (1LL << idx);
-            optCnt += operations[idx];
+            k -= (1LL << (idx - 1));
+            optCnt += operations[idx - 1];
         }
     }
 
