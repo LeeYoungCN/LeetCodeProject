e22a54a466822212aaed09bc2a0fe029d9426415
LY_WSL <1175529741@qq.com>
2025-08-05 14:40:00 +0800

    [test] add build time print

--- code/test/main.cpp
+++ code/test/main.cpp
@@ -4,7 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Running main() from %s\n", __FILE__);
+    printf("Running main() from: %s.\n", __FILE__);
+    printf("Build time: %s.\n", __TIME__);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
